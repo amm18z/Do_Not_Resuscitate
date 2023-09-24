3d2c5dc6d7becc0610f4018d6471d00029c33b00
Aiden <118379307+awa03@users.noreply.github.com>
2023-09-24 17:58:24 -0400

    fixCode-fixDoor
    - Add Door Layer
    - Comment Code
    - Animate TV
    - Animate Bed

--- Inoculation/Assets/Menu Scripts/Player_Movement_Script.cs
+++ Inoculation/Assets/Menu Scripts/Player_Movement_Script.cs
@@ -10,30 +10,34 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed = 2f;
-    public float collisionOffset = 0.05f;
-    private Vector2 movementInput;
-    public ContactFilter2D movementFilter;
-    private Rigidbody2D rb;
+    public float moveSpeed = 2f; // speed serialized
+    public float collisionOffset = 0.05f; // how far the player can be + the collision box
+    private Vector2 movementInput; // movement input
+    public ContactFilter2D movementFilter; // movement filter
+    private Rigidbody2D rb; // player rigid body
     
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();   
 
     // Start is called before the first frame update
     void Start()
     {
+        // get the rigid body
         rb = GetComponent<Rigidbody2D>();
     }
     
     private void FixedUpdate() {
         if (movementInput != Vector2.zero)
         {
+            // get position, and movement filter 
             int count = rb.Cast(
                 movementInput,
                 movementFilter,
                 castCollisions,
-                moveSpeed*Time.fixedDeltaTime + collisionOffset);
+                moveSpeed*Time.fixedDeltaTime + collisionOffset); 
+            
             if (count == 0)
             {
+                // change rigid bodys position
                 rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
 
             }
@@ -43,6 +47,7 @@ public class PlayerController : MonoBehaviour
 
     void OnMove(InputValue movementValue)
     {
+        // Set the movement input
         movementInput = movementValue.Get<Vector2>();
        
     }
