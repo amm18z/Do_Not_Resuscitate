01c6fc34f853b28d7493986b93f8e37cc74e4dc2
amm18z <amm18z@my.fsu.edu>
2023-12-14 18:50:11 -0500

    Added Music to Intro Scene
    Also created an AudioManager GameObject and script to control music and later on control sfx

---
+++ Inoculation/Assets/Resources/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("---------- Audio Source ----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    [Header("---------- Audio Clip ----------")]
+    public AudioClip backgroundMusic;
+    public AudioClip sfx1;
+    public AudioClip sfx2;
+    public AudioClip sfx3;
+    public AudioClip sfx4;
+    public AudioClip sfx5;
+
+    private void Start()
+    {
+        musicSource.clip = backgroundMusic;
+        musicSource.Play();
+    }
+}
