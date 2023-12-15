8d5776bf287cde723d49f8a3958e6991b644b446
Smart00th <122415090+Smart00th@users.noreply.github.com>
2023-12-15 03:12:28 -0800

    If working, then this is the finished result of loading screen implementation

--- Inoculation/Assets/moveToMenu.cs
+++ Inoculation/Assets/moveToMenu.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class moveToMenu : MonoBehaviour
 {
-    public int sceneIndex; 
+    public int sceneIndex;
+    public float minimumLoadTime;
+    public GameObject sceneLoad;
+    public GameObject iconLoad;
+    public GameObject screenDisable1;
+    public GameObject screenDisable2;
+    public GameObject screenDisable3;
+    public GameObject screenDisable4;
+    private AsyncOperation asyncLoad;
     public void Move()
     {
-        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single); // Load the shop scene
+        screenDisable1.SetActive(false);
+        screenDisable2.SetActive(false);
+        screenDisable3.SetActive(false);
+        screenDisable4.SetActive(false);
+        StartCoroutine(LoadAsyncScene()); // Load the shop scene
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private IEnumerator LoadAsyncScene()
+    {   
+        sceneLoad.SetActive(true);
+        iconLoad.SetActive(true);
+        float currentLoadTime = 0f;
+        while (currentLoadTime < minimumLoadTime)
+        {
+            currentLoadTime += Time.deltaTime;
+            yield return null;
+        }
+        asyncLoad = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Single); // Load the level scene
+        while (!asyncLoad.isDone)
+        {
+            currentLoadTime += Time.deltaTime;
+            yield return null;
+        }
+
     }
+
+
 }
